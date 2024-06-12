6b4368c0d8d69ce5a80d4190c4911a348bf11fa1
kamileg <kamil.kalicki.dev@gmail.com>
2024-06-12 22:33:56 +0200

    updated templates

--- app/user/urls.py
+++ app/user/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
         name='password_change'
     ),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html'),
+        template_name='password_change_done.html'),
         name='password_change_done'
     ),
     path('password_reset/', auth_views.PasswordResetView.as_view(
